329dc424694309b17af1f406c5f90fcc4e573f7d
Robin Tregaskis <lokidokicoki@gmail.com>
2020-10-03 18:41:11 +0100

    hack in player

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import arcade
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
+SCREEN_TITLE = "Elf Bizniz!"
 
+CHARACTER_SCLAING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
 
 class MyGame(arcade.Window):
     """
@@ -19,11 +22,28 @@ class MyGame(arcade.Window):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        # sprite lists
+        self.coin_list = None
+        self.wall_list = None
+        self.player_list = None
+
+        # holder for player sprite
+        self.player_sprite = None
+
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
-        pass
+        self.player_list = arcade.SpriteList()
+
+        # setup player
+        image_source = ":resources:images/timmy_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCLAING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+
 
     def on_draw(self):
         """ Render the screen. """
@@ -31,6 +51,7 @@ class MyGame(arcade.Window):
         arcade.start_render()
         # Code to draw the screen goes here
 
+        self.player_list.draw()
 
 def main():
     """ Main method """
