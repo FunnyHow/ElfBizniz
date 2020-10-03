4fef4e1237e6d8121bc248ad301f64dd6b0be64b
Robin Tregaskis <lokidokicoki@gmail.com>
2020-10-03 19:16:59 +0100

    key handler

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Elf Bizniz!"
 
-CHARACTER_SCLAING = 1
+CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
+PLAYER_MOVEMENT_SPEED = 5
+
 
 class MyGame(arcade.Window):
     """
@@ -18,7 +20,6 @@ class MyGame(arcade.Window):
     """
 
     def __init__(self):
-
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
@@ -29,21 +30,30 @@ class MyGame(arcade.Window):
 
         # holder for player sprite
         self.player_sprite = None
+        self.physics_engine = None
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
         self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
 
         # setup player
         image_source = "images/timmy_idle.png"
-        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCLAING)
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
         self.player_list.append(self.player_sprite)
 
+        # walls
+        for x in range(0, 1000, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
 
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list)
 
     def on_draw(self):
         """ Render the screen. """
@@ -52,6 +62,35 @@ class MyGame(arcade.Window):
         # Code to draw the screen goes here
 
         self.player_list.draw()
+
+    def on_key_press(self, key: int, modifiers: int):
+        """key handler"""
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key: int, modifiers: int):
+        """key handler"""
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time: float):
+        """movement and game logic"""
+
+        # move player with physics!
+        self.physics_engine.update()
+
 
 def main():
     """ Main method """
